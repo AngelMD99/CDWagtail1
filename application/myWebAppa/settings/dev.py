4523f6f385df5e723139aed41fef5c5001d53bd5
AngelMD99 <angelm.duarte@hotmail.com>
2021-08-23 17:43:38 -0600

    Commit inicial de la aplicacion
    Aplicacion inicializada

---
+++ application/myWebAppa/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-xfuz#(23sw88hd9u(i@8+yij#us&1puw8yqer5+h+xl_vuw6ke'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
